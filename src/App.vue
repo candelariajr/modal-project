8b2174472981c4d0970d3dc7a224ab90318002cb
candelariajr <candelariajr@appstate.edu>
2021-01-21 14:25:32 -0500

    Modal

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,79 @@
+<!--Removing Boilerplate-->
 <template>
+  <!--
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  -->
+  <h1>{{ title}}</h1><br><br>
+  <input type="text" ref="name"><br><br>
+  <button @click="handleClick">CLICK</button>
+  <hr>
+  <!-- Invoke the Modal component -->
+  <!-- <Modal header="Sign up for The Thing" text="Get some free ninja swag!"/> -->
+  <!-- Replace it with proper data binding -->
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :theme="theme" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign Up Now!</a>
+        <a href="#">Click here for more info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Get your ninja swag!</p>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal :theme="theme" @close="toggleModal2">
+      <h1>Modal 2 Header!</h1>
+      <template v-slot:links>
+        {{modal2text}}
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Open Modal</button><br><br>
+  <button @click="toggleModal2">Open Modal2</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue';
+import Modal from './components/Modal';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Modal},
+  data(){
+    return{
+      title: "First Vue CLI App",
+      header: 'Sign up for The Thing',
+      text: 'Get some free ninja swag!',
+      showModal: false,
+      theme: 'nosale',
+      showModal2: false,
+      modal2text: "This is Modal 2"
+    }
+  },
+  methods: {
+    handleClick(){
+      //refs get the ref ELEMENT
+      //alert(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
+    toggleModal(){
+      this.showModal = !this.showModal;
+    },
+    toggleModal2(){
+      this.showModal2 = !this.showModal2;
+    }
   }
 }
 </script>
 
 <style>
+*{
+  user-select: none;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +82,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+
+.active{
+  background-color:cyan;
+}
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{sale: theme ==='sale'}">
+      <slot>Default Content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  //This brings in the value from App.vue into the scope of this component
+  //Props are not needed if the values are being passed in the form of a template passed through
+  //to the component with a slot
+  props: ['theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style>
+/*
+NOTE- regular CSS works as well
+ */
+.modal{
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop{
+  top: 0;
+  position: fixed;
+  background: rgba(0,0,0,0.5);
+  width: 100%;
+  height:100%;
+}
+
+.modal.h1{
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+
+.modal.sale{
+  background: crimson;
+  color: white;
+}
+
+.modal.actions{
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+
+.modal .actions a{
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.modal.sale h1{
+  color: white;
+}
+</style>
